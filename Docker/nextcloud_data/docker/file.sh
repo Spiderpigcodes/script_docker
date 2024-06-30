0ba1e12fa6f56d84a51fe486fbc30a68c17c7cf6
Spiderpigcodes <131910136+Spiderpigcodes@users.noreply.github.com>
2024-06-30 15:12:35 +0200

    Update file.sh

--- Docker/nextcloud_data/docker/file.sh
+++ Docker/nextcloud_data/docker/file.sh
@@ -1,2 +1,3 @@
 sudo chown www-data:www-data /mnt/shared_nvme/nextcloud_data/docker/nginx.conf
 sudo chmod 644 /mnt/shared_nvme/nextcloud_data/docker/nginx.conf
+sudo chmod -R 777 /mnt/shared_nvme/nextcloud_data
